8adcad3ec4a83d88c640fc3edc7b9d330884ad4f
PainKiller995 <fahdakramdaher@gmail.com>
2024-12-06 16:44:46 +0100

    add providers

---
+++ components/providers/app-providers.tsx
@@ -0,0 +1,15 @@
+"use-client";
+
+import { ThemeProvider } from "next-themes";
+
+interface AppProvidersProps {
+  children: React.ReactNode;
+}
+
+export function AppProviders({ children }: AppProvidersProps) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </>
+  );
+}
