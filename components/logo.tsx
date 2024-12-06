78ca7d8e9316f51f97d1da637e0c419a89df0b1c
PainKiller995 <fahdakramdaher@gmail.com>
2024-12-06 15:59:13 +0100

    add sidebar

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { SquareDashedMousePointer } from "lucide-react";
 
-const Logo = () => {
-  return <div>Logo</div>;
+interface LogoProps {
+  fontSize?: string;
+  iconSize?: number;
+}
+const Logo = ({ fontSize = "text-2xl", iconSize = 20 }: LogoProps) => {
+  return (
+    <Link
+      href="/"
+      className={cn(
+        "text-2xl font-extrabold flex items-center gap-2",
+        fontSize
+      )}
+    >
+      <div className="rounded-xl bg-gradient-to-r from-emerald-500 to-emerald-600 p-2">
+        <SquareDashedMousePointer size={iconSize} className="stroke-white" />
+      </div>
+      <span className="bg-gradient-to-r from-emerald-500 to-emerald-600 bg-clip-text text-transparent">
+        Auto
+      </span>
+      <span className="text-stone-700 dark:text-stone-300">Flow</span>
+    </Link>
+  );
 };
 
 export default Logo;
