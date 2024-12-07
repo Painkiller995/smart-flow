26f9f2e9893916205bae8e30a6d7bf94994f726b
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-08 00:15:12 +0100

    update layout remove non used components

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,5 @@
 "use client";
-
-import BreadcrumbHeader from "@/components/breadcrumb-header";
 import Logo from "@/components/logo";
-import DesktopSidebar from "@/components/sidebar/desktop-sidebar";
-import MobileSidebar from "@/components/sidebar/mobile-sidebar";
-import { ThemeModeToggle } from "@/components/theme-mode-toggle";
-import { Separator } from "@/components/ui/separator";
 
 function layout({
   children,
