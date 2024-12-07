a38984ca79f6ad1a3b60c781e9783d472b4b09d6
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-07 23:47:18 +0100

    tune mobile sidebar

--- components/sidebar/mobile-sidebar.tsx
+++ components/sidebar/mobile-sidebar.tsx
@@ -6,31 +6,36 @@ import { routes } from "@/config/routes";
 import Link from "next/link";
 import { Button, buttonVariants } from "../ui/button";
 import { usePathname } from "next/navigation";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { MenuIcon } from "lucide-react";
 
 const MobileSidebar = () => {
-  const [isOpen, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
   const activeRoute = routes.find((route) => pathname.includes(route.href));
 
   return (
     <div className="block border-separate bg-background md:hidden">
       <nav className="container flex items-center justify-between px-8">
-        <Sheet open={isOpen} onOpenChange={setOpen}>
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
           <SheetTrigger asChild>
-            <Button variant="ghost" size="icon">
-              <MenuIcon />
-            </Button>
+            <SheetTitle>
+              <Button variant="ghost" size="icon">
+                <MenuIcon />
+              </Button>
+            </SheetTitle>
           </SheetTrigger>
           <SheetContent
             side="left"
             className="w-[400px] sm:w-[540px] space-y-4"
           >
-            <div className="flex border-b-[1px] border-separate p-4">
-              <Logo />
-            </div>
-            <div className="flex flex-col p-2">
+            <Logo />
+            <div className="flex flex-col gap-1">
               {routes.map((route) => (
                 <Link
                   key={route.href}
@@ -41,6 +46,7 @@ const MobileSidebar = () => {
                         ? "sidebarActiveItem"
                         : "sidebarItem",
                   })}
+                  onClick={() => setIsOpen(false)}
                 >
                   <route.icon size={20} /> {route.label}
                 </Link>
