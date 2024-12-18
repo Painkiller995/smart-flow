def00f077dfa89236856e671981cb3921872933d
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-18 14:06:48 +0100

    add styling to the check icon

--- app/workflow/_components/top-bar/save-button.tsx
+++ app/workflow/_components/top-bar/save-button.tsx
@@ -1,19 +1,21 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useReactFlow } from "@xyflow/react";
 import { CheckIcon } from "lucide-react";
 import React from "react";
 
 const SaveButton = () => {
+  const { toObject } = useReactFlow();
   return (
     <Button
       variant="outline"
       className="flex items-center gap-2"
       onClick={() => {
-        alert("Todo");
+        console.log(toObject());
       }}
     >
-      <CheckIcon size={16} />
+      <CheckIcon size={16} className="stroke-green-400" />
       Save
     </Button>
   );
