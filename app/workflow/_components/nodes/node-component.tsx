01d4e035acb93f8a9eb396180eae1ab89a822d1e
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-20 02:48:12 +0100

    add delete and copy button

--- app/workflow/_components/nodes/node-component.tsx
+++ app/workflow/_components/nodes/node-component.tsx
@@ -1,18 +1,18 @@
-import { NodeProps } from "@xyflow/react";
-import { memo } from "react";
-import NodeCard from "./node-card";
-import NodeHeader from "./node-header";
-import { AppNodeData } from "@/types/app-node";
-import { TaskRegistry } from "@/lib/workflow/task/registry";
-import NodeInputs, { NodeInput } from "./node-inputs";
-import NodeOutputs, { NodeOutput } from "./node-outputs";
+import { TaskRegistry } from '@/lib/workflow/task/registry';
+import { AppNodeData } from '@/types/app-node';
+import { NodeProps } from '@xyflow/react';
+import { memo } from 'react';
+import NodeCard from './node-card';
+import NodeHeader from './node-header';
+import NodeInputs, { NodeInput } from './node-inputs';
+import NodeOutputs, { NodeOutput } from './node-outputs';
 
 const NodeComponent = memo((props: NodeProps) => {
   const nodeData = props.data as AppNodeData;
   const task = TaskRegistry[nodeData.type];
   return (
     <NodeCard nodeId={props.id} isSelected={!!props.selected}>
-      <NodeHeader taskType={nodeData.type} />
+      <NodeHeader nodeId={props.id} taskType={nodeData.type} />
       <NodeInputs>
         {task.inputs.map((input) => (
           <NodeInput key={input.name} input={input} nodeId={props.id} />
@@ -28,4 +28,4 @@ const NodeComponent = memo((props: NodeProps) => {
 });
 
 export default NodeComponent;
-NodeComponent.displayName = "NodeComponent";
+NodeComponent.displayName = 'NodeComponent';
--- app/workflow/_components/nodes/node-header.tsx
+++ app/workflow/_components/nodes/node-header.tsx
@@ -1,28 +1,56 @@
-"use client";
+'use client';
 
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { TaskRegistry } from "@/lib/workflow/task/registry";
-import { TaskType } from "@/types/task";
-import { CoinsIcon, GripVerticalIcon } from "lucide-react";
-import React from "react";
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { CreateFlowNode } from '@/lib/workflow/create-flow-node';
+import { TaskRegistry } from '@/lib/workflow/task/registry';
+import { AppNode } from '@/types/app-node';
+import { TaskType } from '@/types/task';
+import { useReactFlow } from '@xyflow/react';
+import { CoinsIcon, CopyIcon, GripVerticalIcon, TrashIcon } from 'lucide-react';
 
-const NodeHeader = ({ taskType }: { taskType: TaskType }) => {
+const NodeHeader = ({ taskType, nodeId }: { taskType: TaskType; nodeId: string }) => {
   const task = TaskRegistry[taskType];
+  const { deleteElements, getNode, addNodes } = useReactFlow();
   return (
     <div className="flex items-center gap-2 p-2">
       <task.icon size={30} />
-      <div className="flex justify-between items-center w-full">
-        <p className="text-xs font-bold uppercase text-muted-foreground">
-          {task.label}
-        </p>
+      <div className="flex w-full items-center justify-between">
+        <p className="text-xs font-bold uppercase text-muted-foreground">{task.label}</p>
       </div>
-      <div className="flex gap-1 items-center">
+      <div className="flex items-center gap-1">
         {task.isEntryPoint && <Badge>Entry</Badge>}
-        <Badge className="gap-2 flex items-center text-xs">
+        <Badge className="flex items-center gap-2 text-xs">
           <CoinsIcon size={16}></CoinsIcon>
           TODO
         </Badge>
+        {!task.isEntryPoint && (
+          <>
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => {
+                const node = getNode(nodeId) as AppNode;
+                if (!node) return;
+                const newX = node.position.x;
+                const newY = node.position.y + node.measured?.height! + 20;
+                const newNode = CreateFlowNode(node.data.type, { x: newX, y: newY });
+                addNodes([newNode]);
+              }}
+            >
+              <CopyIcon size={12} />
+            </Button>
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => {
+                deleteElements({ nodes: [{ id: nodeId }] });
+              }}
+            >
+              <TrashIcon size={12} />
+            </Button>
+          </>
+        )}
         <Button variant="ghost" size="icon" className="drag-handle cursor-grab">
           <GripVerticalIcon size={20} />
         </Button>
