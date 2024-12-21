124583bc60889e3cac5d88018e1e0151f6108db6
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-21 02:22:55 +0100

    add getInvalidInputs

--- lib/workflow/execution-plan.ts
+++ lib/workflow/execution-plan.ts
@@ -1,6 +1,6 @@
 import { AppNode } from "@/types/app-node";
-import { WorkflowExecutionPlan } from "@/types/workflow";
-import { Edge } from "@xyflow/react";
+import { WorkflowExecutionPlan, WorkflowExecutionPlanPhase } from "@/types/workflow";
+import { Edge, getIncomers } from "@xyflow/react";
 import { TaskRegistry } from "./task/registry";
 
 type FlowToExecutionPlanType = {
@@ -15,6 +15,8 @@ export function FlowToExecutionPlan(nodes: AppNode[], edges: Edge[]): FlowToExec
         throw new Error("TODO:Handle this error")
     }
 
+    const planned = new Set<string>()
+
     const executionPlan: WorkflowExecutionPlan = [
         {
             phase: 1,
@@ -22,6 +24,70 @@ export function FlowToExecutionPlan(nodes: AppNode[], edges: Edge[]): FlowToExec
         }
     ]
 
+    for (let phase = 2; phase <= nodes.length || planned.size < nodes.length; phase++) {
+        const nextPhase: WorkflowExecutionPlanPhase = { phase, nodes: [] }
+
+        for (const currentNode of nodes) {
+
+            if (planned.has(currentNode.id)) {
+                // Node already put in the execution plan
+                continue
+            }
+
+            const invalidInputs = getInvalidInputs(currentNode, edges, planned)
+
+            if (invalidInputs.length > 0) {
+                const incomers = getIncomers(currentNode, nodes, edges)
+                if (incomers.every((incomer) => planned.has(incomer.id))) {
+                    console.error("Invalid input", currentNode.id, invalidInputs)
+                    throw new Error("TODO: Handle Error 1")
+                } else { continue }
+            }
+
+            nextPhase.nodes.push(currentNode)
+            planned.add(currentNode.id)
+        }
+    }
+
     return { executionPlan }
 
-}  
+}
+
+
+function getInvalidInputs(node: AppNode, edges: Edge[], planned: Set<string>) {
+
+    const invalidInputs = []
+
+    const inputs = TaskRegistry[node.data.type].inputs
+
+    for (const input of inputs) {
+        const inputValue = node.data.inputs[input.name]
+        const inputValueProvided = inputValue?.length > 0
+
+        if (inputValueProvided) {
+            continue
+        }
+
+        const incomingEdges = edges.filter((edge) => edge.target === node.id)
+
+        const inputLinkedToOutput = incomingEdges.find((edge) => edge.targetHandle === input.name)
+
+        const requiredInputProvidedByVisitedOutput = input.require && inputLinkedToOutput && planned.has(inputLinkedToOutput.source)
+
+        if (requiredInputProvidedByVisitedOutput) {
+            continue
+        }
+        else if (!input.require) {
+            if (!inputLinkedToOutput) {
+                continue
+            }
+            if (inputLinkedToOutput && planned.has(inputLinkedToOutput.source)) {
+                continue
+            }
+        }
+
+        invalidInputs.push(input.name)
+
+    }
+    return invalidInputs
+}
--- types/workflow.ts
+++ types/workflow.ts
@@ -17,7 +17,10 @@ export type WorkflowTask = {
     credits: number
 }
 
-export type WorkflowExecutionPlan = {
+export type WorkflowExecutionPlanPhase = {
     phase: number
     nodes: AppNode[]
-}[]
+}
+export type WorkflowExecutionPlan = WorkflowExecutionPlanPhase[]
+
+
