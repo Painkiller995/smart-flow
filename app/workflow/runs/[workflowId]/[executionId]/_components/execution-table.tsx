784dda1d5340376fb7c996cd2866de4826bd49ea
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-23 23:49:09 +0100

    add ExecutionsTable component

--- app/workflow/runs/[workflowId]/[executionId]/_components/execution-table.tsx
+++ app/workflow/runs/[workflowId]/[executionId]/_components/execution-table.tsx
@@ -8,20 +8,22 @@ async function ExecutionsTable({ workflowId }: { workflowId: string }) {
     return <div className="">No data</div>;
   }
 
-  if (executions.length == 0) {
-    <div className="container w-full py-6">
-      <div>
-        <div>
-          <InboxIcon size={40} className="stroke-primary" />
-        </div>
-        <div className="">
-          <p className="font-bold">No runs have been triggered yet for this workflow</p>
-          <p className="text-sm text-muted-foreground">
-            You can trigger a new run in the editor page
-          </p>
+  if (executions.length === 0) {
+    return (
+      <div className="container w-full py-6">
+        <div className="flex h-full w-full flex-col items-center justify-center gap-2">
+          <div className="flex h-20 w-20 items-center justify-center rounded-full bg-accent">
+            <InboxIcon size={40} className="stroke-primary" />
+          </div>
+          <div className="flex flex-col gap-1 text-center">
+            <p className="font-bold">No runs have been triggered yet for this workflow</p>
+            <p className="text-sm text-muted-foreground">
+              You can trigger a new run in the editor page
+            </p>
+          </div>
         </div>
       </div>
-    </div>;
+    );
   }
 
   return <pre className=""> {JSON.stringify(executions, null, 4)}</pre>;
