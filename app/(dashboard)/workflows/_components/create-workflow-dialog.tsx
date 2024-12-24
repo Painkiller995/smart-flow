377a419e3ee30a4dbea0c9e9f58ab3196543edd8
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-24 01:10:35 +0100

    improve create workflow schema

--- app/(dashboard)/workflows/_components/create-workflow-dialog.tsx
+++ app/(dashboard)/workflows/_components/create-workflow-dialog.tsx
@@ -1,22 +1,14 @@
-"use client";
+'use client';
 
-import React, { useCallback, useState } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
-import { Layers2Icon, Loader2 } from "lucide-react";
-import CustomDialogHeader from "./custom-dialog-header";
-import { useForm } from "react-hook-form";
-import {
-  createWorkflowSchema,
-  createWorkflowSchemaType,
-} from "@/schema/workflow";
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogDescription, DialogTrigger } from '@/components/ui/dialog';
+import { createWorkflowSchema, createWorkflowSchemaType } from '@/schema/workflow';
+import { Layers2Icon, Loader2 } from 'lucide-react';
+import { useCallback, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import CustomDialogHeader from './custom-dialog-header';
 
-import { zodResolver } from "@hookform/resolvers/zod";
+import { CreateWorkflow } from '@/actions/workflows/create-workflow';
 import {
   Form,
   FormControl,
@@ -25,12 +17,12 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { useMutation } from "@tanstack/react-query";
-import { CreateWorkflow } from "@/actions/workflows/create-workflow";
-import { toast } from "sonner";
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
 
 interface CreateWorkflowDialogProps {
   triggerText?: string;
@@ -41,22 +33,25 @@ const CreateWorkflowDialog = ({ triggerText }: CreateWorkflowDialogProps) => {
 
   const form = useForm<createWorkflowSchemaType>({
     resolver: zodResolver(createWorkflowSchema),
-    defaultValues: {},
+    defaultValues: {
+      name: '',
+      description: '',
+    },
   });
 
   const { mutate, isPending } = useMutation({
     mutationFn: CreateWorkflow,
     onSuccess: () => {
-      toast.success("Workflow created", { id: "create-workflow" });
+      toast.success('Workflow created', { id: 'create-workflow' });
     },
     onError: () => {
-      toast.error("Failed to create the workflow", { id: "create-workflow" });
+      toast.error('Failed to create the workflow', { id: 'create-workflow' });
     },
   });
 
   const onSubmit = useCallback(
     (values: createWorkflowSchemaType) => {
-      toast.loading("Creating new workflow...", { id: "create-workflow" });
+      toast.loading('Creating new workflow...', { id: 'create-workflow' });
       mutate(values);
     },
     [mutate]
@@ -71,7 +66,7 @@ const CreateWorkflowDialog = ({ triggerText }: CreateWorkflowDialogProps) => {
       }}
     >
       <DialogTrigger asChild>
-        <Button>{triggerText ?? "Create workflow"}</Button>
+        <Button>{triggerText ?? 'Create workflow'}</Button>
       </DialogTrigger>
       <DialogContent className="px-2">
         <CustomDialogHeader
@@ -81,24 +76,19 @@ const CreateWorkflowDialog = ({ triggerText }: CreateWorkflowDialogProps) => {
         />
         <div className="p-6">
           <Form {...form}>
-            <form
-              className="space-y-8 w-full"
-              onSubmit={form.handleSubmit(onSubmit)}
-            >
+            <form className="w-full space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
               <FormField
                 control={form.control}
                 name="name"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel className="flex gap-1 items-center">
+                    <FormLabel className="flex items-center gap-1">
                       Name:<p className="text-xs text-primary">(required)</p>
                     </FormLabel>
                     <FormControl>
                       <Input {...field} />
                     </FormControl>
-                    <FormDescription>
-                      Choose a descriptive and unique name
-                    </FormDescription>
+                    <FormDescription>Choose a descriptive and unique name</FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
@@ -108,11 +98,9 @@ const CreateWorkflowDialog = ({ triggerText }: CreateWorkflowDialogProps) => {
                 name="description"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel className="flex gap-1 items-center">
+                    <FormLabel className="flex items-center gap-1">
                       Description:
-                      <p className="text-xs text-muted-foreground">
-                        (optional)
-                      </p>
+                      <p className="text-xs text-muted-foreground">(optional)</p>
                     </FormLabel>
                     <FormControl>
                       <Textarea className="resize-none" {...field} />
@@ -123,7 +111,7 @@ const CreateWorkflowDialog = ({ triggerText }: CreateWorkflowDialogProps) => {
                 )}
               />
               <Button type="submit" className="w-full" disabled={isPending}>
-                {!isPending && "Proceed"}
+                {!isPending && 'Proceed'}
                 {isPending && <Loader2 className="animate-spin" />}
               </Button>
             </form>
--- schema/workflow.ts
+++ schema/workflow.ts
@@ -1,7 +1,10 @@
 import { z } from "zod"
 
 export const createWorkflowSchema = z.object({
-    name: z.string().max(50),
+    name: z
+        .string()
+        .min(1, { message: "Name must have at least 1 character." })
+        .max(50, { message: "Name must not exceed 50 characters." }),
     description: z.string().max(80).optional()
 })
 
