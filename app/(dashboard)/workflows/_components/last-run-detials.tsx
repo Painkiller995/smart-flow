25aa8020a24148a6f086e1d1937a2b86deb13353
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-25 22:45:14 +0100

    add next run details

--- app/(dashboard)/workflows/_components/last-run-detials.tsx
+++ app/(dashboard)/workflows/_components/last-run-detials.tsx
@@ -1,14 +1,17 @@
 import ExecutionStatusIndicator from '@/app/workflow/runs/[workflowId]/[executionId]/_components/execution-status-indicator';
 import { WorkflowExecutionStatus } from '@/types/workflow';
 import { Workflow } from '@prisma/client';
-import { formatDistanceToNow } from 'date-fns';
-import { ChevronRightIcon } from 'lucide-react';
+import { format, formatDistanceToNow } from 'date-fns';
+import { formatInTimeZone } from 'date-fns-tz';
+import { ChevronRightIcon, ClockIcon } from 'lucide-react';
 import Link from 'next/link';
 
 const LastRunDetails = ({ workflow }: { workflow: Workflow }) => {
-  const { lastRunId, lastRunStatus, lastRunAt } = workflow;
+  const { lastRunId, lastRunStatus, lastRunAt, nextRunAt } = workflow;
 
   const formatStartedAt = lastRunAt && formatDistanceToNow(lastRunAt, { addSuffix: true });
+  const nextSchedule = nextRunAt && format(nextRunAt, 'yyyy-MM-dd HH:mm');
+  const nextScheduleUTC = nextRunAt && formatInTimeZone(nextRunAt, 'UTC', 'HH:mm');
 
   return (
     <div className="flex items-center justify-between bg-primary/5 px-4 py-1 text-muted-foreground">
@@ -28,7 +31,16 @@ const LastRunDetails = ({ workflow }: { workflow: Workflow }) => {
             />
           </Link>
         )}
+        {!lastRunAt && <p>No runs yet</p>}
       </div>
+      {nextRunAt && (
+        <div className="flex items-center gap-2 text-sm">
+          <ClockIcon size={16} />
+          <span>Next run at:</span>
+          <span>{nextSchedule}</span>
+          <span>{nextScheduleUTC} UTC</span>
+        </div>
+      )}
     </div>
   );
 };
