4a218f8038b34c445e247ab550c7402f5f247345
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-25 23:34:26 +0100

    add DuplicateWorkflow action

---
+++ actions/workflows/duplicate-workflow.ts
@@ -0,0 +1,48 @@
+"use server"
+
+import prisma from "@/lib/prisma"
+import { duplicateWorkflowSchema, duplicateWorkflowSchemaType } from "@/schema/workflow"
+import { WorkflowStatus } from "@/types/workflow"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+export async function DuplicateWorkflow(form: duplicateWorkflowSchemaType) {
+
+    const { success, data } = duplicateWorkflowSchema.safeParse(form)
+
+    if (!success) {
+        throw new Error("invalid form data")
+    }
+
+    const { userId } = await auth()
+
+    if (!userId) {
+        throw new Error("unauthenticated")
+    }
+
+    const sourceWorkflow = await prisma.workflow.findUnique({
+        where: {
+            id: data.workflowId
+        }
+    })
+
+    if (!sourceWorkflow) {
+        throw new Error("Workflow not found")
+    }
+
+    const result = await prisma.workflow.create({
+        data: {
+            userId,
+            name: data.name,
+            description: data.description,
+            status: WorkflowStatus.DRAFT,
+            definition: sourceWorkflow.definition,
+        }
+    })
+
+    if (!result) {
+        throw new Error("Failed to create workflow")
+    }
+
+    redirect(`/workflow/editor/${result.id}`)
+}   
---
+++ app/(dashboard)/workflows/_components/duplicate-workflow-dialog.tsx
@@ -0,0 +1,121 @@
+'use client';
+
+import { DuplicateWorkflow } from '@/actions/workflows/duplicate-workflow';
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogDescription, DialogTrigger } from '@/components/ui/dialog';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { duplicateWorkflowSchema, duplicateWorkflowSchemaType } from '@/schema/workflow';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { Layers2Icon, Loader2 } from 'lucide-react';
+import { useCallback, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import CustomDialogHeader from './custom-dialog-header';
+
+interface DuplicateWorkflowDialogProps {
+  triggerText?: string;
+}
+
+const DuplicateWorkflowDialog = ({ triggerText }: DuplicateWorkflowDialogProps) => {
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<duplicateWorkflowSchemaType>({
+    resolver: zodResolver(duplicateWorkflowSchema),
+    defaultValues: {
+      name: '',
+      description: '',
+    },
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: DuplicateWorkflow,
+    onSuccess: () => {
+      toast.success('Workflow duplicated', { id: 'duplicate-workflow' });
+    },
+    onError: () => {
+      toast.error('Failed to duplicate the workflow', { id: 'duplicate-workflow' });
+    },
+  });
+
+  const onSubmit = useCallback(
+    (values: duplicateWorkflowSchemaType) => {
+      toast.loading('Duplicating workflow...', { id: 'duplicate-workflow' });
+      mutate(values);
+    },
+    [mutate]
+  );
+
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={(open) => {
+        form.reset();
+        setOpen(open);
+      }}
+    >
+      <DialogTrigger asChild>
+        <Button>Duplicate workflow</Button>
+      </DialogTrigger>
+      <DialogContent className="px-2">
+        <CustomDialogHeader title="Duplicate workflow" icon={Layers2Icon} />
+        <div className="p-6">
+          <Form {...form}>
+            <form className="w-full space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex items-center gap-1">
+                      Name:<p className="text-xs text-primary">(required)</p>
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormDescription>Choose a descriptive and unique name</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex items-center gap-1">
+                      Description:
+                      <p className="text-xs text-muted-foreground">(optional)</p>
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea className="resize-none" {...field} />
+                    </FormControl>
+                    <FormDescription>Provide description</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full" disabled={isPending}>
+                {!isPending && 'Proceed'}
+                {isPending && <Loader2 className="animate-spin" />}
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <DialogDescription>Write something here</DialogDescription>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DuplicateWorkflowDialog;
