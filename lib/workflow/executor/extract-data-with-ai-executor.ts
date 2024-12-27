eedf3e41f33b5e169ef682803fb11b380b2ce76b
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-28 00:27:23 +0100

    add openAI

--- lib/workflow/executor/extract-data-with-ai-executor.ts
+++ lib/workflow/executor/extract-data-with-ai-executor.ts
@@ -1,6 +1,7 @@
 import { symmetricDecrypt } from '@/lib/encryption';
 import prisma from '@/lib/prisma';
 import { ExecutionEnvironment } from '@/types/executor';
+import OpenAI from 'openai';
 import { ExtractDataWithAiTask } from '../task/extract-data-with-ai';
 
 export async function ExtractDataWithAiExecutor(environment: ExecutionEnvironment<typeof ExtractDataWithAiTask>): Promise<boolean> {
@@ -37,8 +38,40 @@ export async function ExtractDataWithAiExecutor(environment: ExecutionEnvironmen
             return false
         }
 
-        console.log(plainCredentialValue)
+        const openai = new OpenAI({
+            apiKey: plainCredentialValue
+        })
 
+        const response = await openai.chat.completions.create({
+            model: "gpt-4o-mini",
+            messages: [
+                {
+                    role: "system",
+                    content: 'You are a webScraper helper that extracts data from HTML or text'
+                },
+                {
+                    role: 'user',
+                    content: content
+                },
+                {
+                    role: 'user',
+                    content: prompt
+                }
+            ],
+            temperature: 1,
+        })
+
+        environment.log.info(`Prompt tokens: ${response.usage?.prompt_tokens}`)
+        environment.log.info(`Completion tokens: ${response.usage?.completion_tokens}`)
+
+        const result = response.choices[0].message.content
+
+        if (!result) {
+            environment.log.error("Empty response form AI")
+            return false
+        }
+
+        environment.setOutput("Extracted data", result)
         return true
     } catch (err: any) {
         environment.log.error(err.message)
