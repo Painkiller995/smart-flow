f32b54a51a721d4c6de984f9773e8ddcc47b5384
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-28 01:29:05 +0100

    add navigate url executor

--- lib/workflow/executor/navigate-url-executor.ts
+++ lib/workflow/executor/navigate-url-executor.ts
@@ -4,13 +4,15 @@ import { NavigateUrlTask } from '../task/navigate-url';
 export async function NavigateUrlExecutor(environment: ExecutionEnvironment<typeof NavigateUrlTask>): Promise<boolean> {
     try {
 
-        const selector = environment.getInput("Selector")
+        const url = environment.getInput("URL")
 
-        if (!selector) {
-            environment.log.error("selector value is not defined")
+        if (!url) {
+            environment.log.error("url is not defined")
         }
 
-        await environment.getPage()!.click(selector)
+        await environment.getPage()!.goto(url)
+        environment.log.info(`Going to ${url}`)
+
         return true
     } catch (err: any) {
         environment.log.error(err.message)
