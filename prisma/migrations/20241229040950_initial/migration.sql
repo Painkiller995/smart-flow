1cfb5df8c861c02cecd588c698cd4cb81d04f562
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-29 05:10:10 +0100

    build a new migration

--- prisma/migrations/20241229040950_initial/migration.sql
+++ prisma/migrations/20241229040950_initial/migration.sql
@@ -74,6 +74,17 @@ CREATE TABLE "Credential" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+-- CreateTable
+CREATE TABLE "UserPurchase" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "stripeId" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "currency" TEXT NOT NULL,
+    "date" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Workflow_name_userId_key" ON "Workflow"("name", "userId");
 
