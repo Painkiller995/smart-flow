ba2615880c11e212d078ba41ae82d14d75bc4005
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-29 03:36:44 +0100

    finish purchase session

--- actions/billing/purchase-credits.ts
+++ actions/billing/purchase-credits.ts
@@ -1,8 +1,11 @@
 "use server"
 
-import prisma from "@/lib/prisma"
-import { PackId } from "@/types/billing"
+import { getAppUrl } from "@/lib/helper/app-url"
+import { stripe } from "@/lib/stripe/stripe"
+import { getCreditsPack, PackId } from "@/types/billing"
 import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
 
 export async function PurchaseCredits(packId: PackId) {
     const { userId } = await auth()
@@ -11,14 +14,36 @@ export async function PurchaseCredits(packId: PackId) {
         throw new Error("unauthenticated")
     }
 
-    const balance = await prisma.userBalance.findUnique({
-        where: {
-            userId
-        }
+    const selectedPack = getCreditsPack(packId)
+
+    if (!selectedPack) {
+        throw new Error("Invalid pack")
+    }
+
+    const priceId = selectedPack.priceId
+
+    const session = await stripe.checkout.sessions.create({
+        mode: 'payment',
+        invoice_creation: {
+            enabled: true
+        },
+        success_url: getAppUrl('billing'),
+        cancel_url: getAppUrl('billing'),
+        metadata: {
+            userId,
+            packId
+        },
+        line_items: [
+            {
+                quantity: 1,
+                price: selectedPack.priceId
+            }
+        ]
     })
 
-    if (!balance) return -1
+    if (!session.url) {
+        throw new Error('Cannot create Stripe session')
+    }
 
-    return balance.credits
-
+    redirect(session.url)
 }  
