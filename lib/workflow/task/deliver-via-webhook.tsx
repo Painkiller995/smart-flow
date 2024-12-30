2807ac6c2ebe9fe10d9879a31c48de6a5021d1c7
Painkiller995 <fahdakramdaher@gmail.com>
2024-12-30 03:00:48 +0100

    fix label

--- lib/workflow/task/deliver-via-webhook.tsx
+++ lib/workflow/task/deliver-via-webhook.tsx
@@ -4,7 +4,7 @@ import { LucideProps, SendIcon } from 'lucide-react';
 
 export const DeliverViaWebhookTask = {
   type: TaskType.DELIVER_VIA_WEBHOOK,
-  label: 'Deliver via webhook task',
+  label: 'Deliver via webhook',
   icon: (props: LucideProps) => <SendIcon className="stroke-blue-400" {...props} />,
   credits: 2,
   inputs: [
