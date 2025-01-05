2b2717d08ac53450e4ae12cf4753148d690e8fc0
Painkiller995 <fahdakramdaher@gmail.com>
2025-01-05 14:29:31 +0100

    fix deletable edge mistake

--- app/workflow/_components/edges/deletable-edge.tsx
+++ app/workflow/_components/edges/deletable-edge.tsx
@@ -1,43 +1,34 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import {
   BaseEdge,
   EdgeLabelRenderer,
   EdgeProps,
   getSmoothStepPath,
   useReactFlow,
-} from "@xyflow/react";
-import React from "react";
+} from '@xyflow/react';
 
 const DeletableEdge = (props: EdgeProps) => {
   const [edgePath, labelX, labelY] = getSmoothStepPath(props);
   const { setEdges } = useReactFlow();
   return (
     <>
-      <BaseEdge
-        path={edgePath}
-        markerEnd={props.markerEnd}
-        style={props.style}
-      />
+      <BaseEdge path={edgePath} markerEnd={props.markerEnd} style={props.style} />
       <EdgeLabelRenderer>
         <div
           style={{
-            position: "absolute",
+            position: 'absolute',
             transform: `translate(-50%,-50%) translate(${labelX}px,${labelY}px)`,
-            pointerEvents: "all",
+            pointerEvents: 'all',
           }}
         >
           <Button
             variant="outline"
             size="icon"
-            className="w-5 h-5 border cursor-pointer rounded-full text-xs leading-none hover:shadow-lg"
+            className="h-5 w-5 cursor-pointer rounded-full border text-xs leading-none hover:shadow-lg"
             onClick={() => {
-              setEdges((edges) =>
-                edges.filter((edge) => {
-                  edge.id !== props.id;
-                })
-              );
+              setEdges((edges) => edges.filter((edge) => edge.id !== props.id));
             }}
           >
             x
