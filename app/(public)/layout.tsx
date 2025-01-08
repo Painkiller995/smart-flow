952f948ae0522a9e161941e56c55b88d34a36fee
Painkiller995 <fahdakramdaher@gmail.com>
2025-01-08 11:00:13 +0100

    make about page public

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import Logo from '@/components/logo';
+import { ThemeModeToggle } from '@/components/theme-mode-toggle';
+import { Separator } from '@/components/ui/separator';
+
+function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen w-full flex-col">
+      <div className="flex items-center justify-between p-2">
+        <Logo iconSize={16} fontSize="text-xl" />
+        <ThemeModeToggle />
+      </div>
+      <Separator />
+      <div className="container py-10">{children}</div>
+    </div>
+  );
+}
+
+export default Layout;
