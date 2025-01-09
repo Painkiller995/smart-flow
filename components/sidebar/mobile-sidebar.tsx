d5e7360309a45bb9babdf69cbdc4dbd29ca5b0b2
Painkiller995 <fahdakramdaher@gmail.com>
2025-01-09 23:53:06 +0100

    fix px issue

--- components/sidebar/mobile-sidebar.tsx
+++ components/sidebar/mobile-sidebar.tsx
@@ -18,7 +18,7 @@ const MobileSidebar = () => {
 
   return (
     <div className="block border-separate bg-background md:hidden">
-      <nav className="container flex items-center justify-between px-8">
+      <nav className="container flex items-center justify-between px-0">
         <Sheet open={isOpen} onOpenChange={setIsOpen}>
           <SheetTrigger asChild>
             <SheetTitle>
