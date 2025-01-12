792447bf39ce55a98927beef2e0be70f1f8363ee
Painkiller995 <fahdakramdaher@gmail.com>
2025-01-12 12:58:29 +0100

    update desktop sidebar

--- components/logo.tsx
+++ components/logo.tsx
@@ -6,16 +6,19 @@ import Link from 'next/link';
 interface LogoProps {
   fontSize?: string;
   iconSize?: number;
+  hideText?: boolean;
 }
-const Logo = ({ fontSize = 'text-2xl', iconSize = 35 }: LogoProps) => {
+const Logo = ({ fontSize = 'text-2xl', iconSize = 35, hideText = false }: LogoProps) => {
   return (
     <Link href="/" className={cn('flex items-center gap-2 text-2xl font-extrabold', fontSize)}>
       <div className="rounded-xl bg-gradient-to-r from-blue-500 to-blue-600 p-2">
         <LogoIcon width={iconSize.toString() || '35'} height={iconSize.toString() || '35'} />
       </div>
-      <div>
-        <span className="text-stone-700 dark:text-stone-300">SmartFlow</span>
-      </div>
+      {!hideText && (
+        <div>
+          <span className="text-stone-700 dark:text-stone-300">SmartFlow</span>
+        </div>
+      )}
     </Link>
   );
 };
--- components/sidebar/desktop-sidebar.tsx
+++ components/sidebar/desktop-sidebar.tsx
@@ -4,8 +4,8 @@ import { routes } from '@/config/routes';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import Logo from '../logo';
+import TooltipWrapper from '../tooltip-wrapper';
 import { buttonVariants } from '../ui/button';
-import UserAvailableBadge from '../user-available-badge';
 
 const DesktopSidebar = () => {
   const pathname = usePathname();
@@ -13,34 +13,32 @@ const DesktopSidebar = () => {
     routes.find((route) => route.href.length > 0 && pathname.includes(route.href)) || routes[0];
 
   return (
-    <div className="relative hidden h-screen w-full min-w-[280px] max-w-[280px] border-separate overflow-hidden border-r-2 bg-primary/5 text-muted-foreground dark:bg-secondary/30 dark:text-foreground md:block">
+    <div className="relative hidden h-screen border-separate overflow-hidden border-r-2 bg-primary/5 text-muted-foreground dark:bg-secondary/30 dark:text-foreground md:block">
       <div className="flex border-separate items-center justify-center gap-2 border-b-[1px] p-4">
-        <Logo />
+        <Logo hideText iconSize={25} />
       </div>
-      <div className="p-2">
-        <UserAvailableBadge />
-      </div>
+
       <div className="flex flex-col justify-start gap-2 p-2">
         {routes.map((route) => (
-          <Link
-            key={route.href}
-            href={`/${route.href}`}
-            className={buttonVariants({
-              variant:
-                activeRoute && activeRoute.href === route.href
-                  ? 'sidebarActiveItem'
-                  : 'sidebarItem',
-            })}
-          >
-            <route.icon
-              style={{
-                height: '25px',
-                width: '25px',
-              }}
-              size={25}
-            />
-            {route.label}
-          </Link>
+          <TooltipWrapper key={route.href} content={route.label} side="right">
+            <Link
+              href={`/${route.href}`}
+              className={buttonVariants({
+                variant:
+                  activeRoute && activeRoute.href === route.href
+                    ? 'sidebarActiveItem'
+                    : 'sidebarItem',
+              })}
+            >
+              <route.icon
+                style={{
+                  height: '25px',
+                  width: '25px',
+                }}
+                size={25}
+              />
+            </Link>
+          </TooltipWrapper>
         ))}
       </div>
     </div>
