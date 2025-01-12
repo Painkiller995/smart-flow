75d9f67f3789d0774b2e61ef09b5de010b19829e
Painkiller995 <fahdakramdaher@gmail.com>
2025-01-12 12:32:54 +0100

    update font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,10 @@ import { AppProviders } from '@/components/providers/app-providers';
 import { signInRoute } from '@/config/routes';
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import { DM_Sans } from 'next/font/google';
 import './globals.css';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
+const font = DM_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'SmartFlow',
@@ -36,7 +27,7 @@ export default function RootLayout({
       }}
     >
       <html lang="en" suppressHydrationWarning>
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body className={font.className}>
           <AppProviders>{children}</AppProviders>
         </body>
       </html>
