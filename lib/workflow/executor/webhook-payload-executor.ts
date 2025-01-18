a4e19011125d76d0d6e9e1db25a00bf8b978ed3b
Painkiller995 <fahdakramdaher@gmail.com>
2025-01-18 19:33:06 +0100

    add WebhookPayloadExecutor

--- lib/workflow/executor/webhook-payload-executor.ts
+++ lib/workflow/executor/webhook-payload-executor.ts
@@ -4,9 +4,16 @@ import { WebhookPayloadTask } from '../task/webhook-payload';
 export async function WebhookPayloadExecutor(environment: ExecutionEnvironment<typeof WebhookPayloadTask>): Promise<boolean> {
     try {
 
-        const jsonData = '{}'
-        const json = JSON.parse(jsonData)
-        environment.setOutput("Webhook payload", '')
+        let payload = environment.getPayload();
+
+        if (!payload) {
+            environment.log.info("Payload is missing. Initializing with an empty JSON object.");
+            payload = {};
+        }
+
+        const formattedPayload = JSON.stringify(payload);
+        environment.setOutput("Webhook payload", formattedPayload);
+
         return true
     } catch (err: any) {
         environment.log.error(err.message)
