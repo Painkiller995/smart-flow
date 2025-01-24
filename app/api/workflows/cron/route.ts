f107ed5280d4480afe6d61a35d4612f913663f5f
Painkiller995 <fahdakramdaher@gmail.com>
2025-01-24 19:10:50 +0100

    change the fetch timeout to one minute

--- app/api/workflows/cron/route.ts
+++ app/api/workflows/cron/route.ts
@@ -50,7 +50,7 @@ function triggerWorkflow(workflowId: string) {
             Authorization: `Bearer ${process.env.API_SECRET!}`
         },
         cache: 'no-store',
-        //signal: AbortSignal.timeout(5000)
+        signal: AbortSignal.timeout(60000) // One minute
     }).catch((err) => {
         console.error(
             "Error triggering workflow with id",
